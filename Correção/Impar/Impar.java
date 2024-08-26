33947ed740cb5cde51ecaa7994b96a35a455979b
luizgaf <luiz.guilhermeaf21@gmail.com>
2024-08-26 10:18:16 -0300

    1

--- Correção/Impar/Impar.java
+++ Correção/Impar/Impar.java
@@ -3,13 +3,14 @@ public class Impar{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
-        int num, result = 0;
+        int num;
+        double result = 0;
         System.out.println("Entre com um numero: ");
         num = sc.nextInt();
 
         for(int i = 0; i < num; i++){
             result += 2*i+1;
         }
-        System.out.println("Resultado "+ result);
+        System.out.printf("Resultado %.0f", result);
     }
 }
