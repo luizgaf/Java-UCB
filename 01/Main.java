41d9d621ba915eef5c722d4dbbf6cc19e14dd08c
luizgaf <luiz.guilhermeaf21@gmail.com>
2024-09-02 10:49:23 -0300

    1

---
+++ 01/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Digite um numero: ");
+		int num1 = sc.nextInt();
+
+		System.out.println("Digite outro numero: ");
+		int num2 = sc.nextInt();	
+
+		System.out.printf("%d + %d = %d%n", num1, num2, num1+num2);
+		System.out.printf("Raiz de %d = %.2f%n", num1, Math.sqrt(num1));
+		System.out.printf("Quadrado de %d = %.2f", num2, Math.pow(num2, 2));	
+	}
+}
