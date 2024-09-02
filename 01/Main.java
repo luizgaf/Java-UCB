dfe403d8abb985dbd5dd75965ece79d4947a4bcf
luizgaf <luiz.guilhermeaf21@gmail.com>
2024-09-02 11:31:33 -0300

    1

--- 01/Main.java
+++ 01/Main.java
@@ -13,5 +13,6 @@ public class Main{
 		System.out.printf("%d + %d = %d%n", num1, num2, num1+num2);
 		System.out.printf("Raiz de %d = %.2f%n", num1, Math.sqrt(num1));
 		System.out.printf("Quadrado de %d = %.2f", num2, Math.pow(num2, 2));	
+		
 	}
 }
